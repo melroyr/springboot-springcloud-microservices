f1f59dcb4f6905def6329ff879495b6258dee9d2
emretterzi <56559417+emretterzi@users.noreply.github.com>
2024-06-18 18:21:40 +0300

    PomXML update

--- order-services/src/main/java/org.example.orderservice/config/WebClientConfig.java
+++ order-services/src/main/java/org.example.orderservice/config/WebClientConfig.java
@@ -1,4 +1,18 @@
 package org.example.orderservice.config;
 
-public class WebClient {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().baseUrl("http://localhost:8080").build();
+    }
+
+
 }
--- order-services/src/main/java/org.example.orderservice/service/Impl/OrderServiceImpl.java
+++ order-services/src/main/java/org.example.orderservice/service/Impl/OrderServiceImpl.java
@@ -9,17 +9,20 @@ import org.example.orderservice.model.OrderLineItems;
 import org.example.orderservice.repository.OrderRepository;
 import org.example.orderservice.service.OrderService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Transactional //tüm operasyonlar ya tamamen başarılı olur ya da tamamen geri alınır (rollback). Bu sayede veri tutarsızlıkları önlenir.
+@Transactional
+//tüm operasyonlar ya tamamen başarılı olur ya da tamamen geri alınır (rollback). Bu sayede veri tutarsızlıkları önlenir.
 //sipariş kaydet ,ödeme tamamla , , stok güncelle ; Atomiklik, izolasyon, tutarlılık ve dayanıklılık (ACID) prensipleri
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
 
     @Override
@@ -27,14 +30,30 @@ public class OrderServiceImpl implements OrderService {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-       List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDto)
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        Boolean result =webClient.get() //client oluşturuldu
+                .uri("http://localhost:8080/api/inventory")//inventory servisine istek atılıyor
+                .retrieve() //response alınır
+                .bodyToMono(Boolean.class) //bodyToMono ile dönüş tipi belirlenir
+                .block(); //block ile response beklenir ve sonuç döner response beklenilmesi lazım çünkü stok kontrolü yapılacak
+
+
+
+        if(result){
+            orderRepository.save(order);
+
+        }
+        else {
+            throw new IllegalArgumentException("Product is out of stock");
+        }
+
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
