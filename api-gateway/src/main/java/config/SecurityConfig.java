119bc98eaca373f524bc7f37b37e2a41b5385870
silasener <slaasener@gmail.com>
2024-06-28 14:34:30 +0300

    comment line added

--- api-gateway/src/main/java/config/SecurityConfig.java
+++ api-gateway/src/main/java/config/SecurityConfig.java
@@ -13,15 +13,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-    serverHttpSecurity.csrf(csrf -> csrf.disable())
-            .authorizeExchange(exchange -> exchange
-                    .pathMatchers("/eureka/**")
-                    .permitAll()
-                    .anyExchange()
-                    .authenticated())
-            .oauth2ResourceServer((oauth2) -> oauth2
-                .jwt(Customizer.withDefaults()));
+        serverHttpSecurity.csrf(csrf -> csrf.disable()) //CSRF (Cross-Site Request Forgery), güvenlik saldırılarına karşı koruma sağlar, devre dışı bırakıldı
+                .authorizeExchange(exchange -> exchange // Endpoint'lerin erişimini yapılandırır.
+                        .pathMatchers("/eureka/**") //"eureka" path pattern'ine sahip olan isteklerin konfigürasyonunu yapılandırır.
+                        .permitAll() ///eureka/** pattern'ine sahip  isteklerin kimlik doğrulaması olmadan erişimine izin verir.
+                        .anyExchange() //Diğer tüm endpoint'ler için aynı konfigürasyonu belirtir.
+                        .authenticated()) // Kimlik doğrulaması gerektiren tüm endpoint'ler için erişimi sınırlar
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())); //gelen HTTP isteklerinde JWT'nin içeriğinde yer alan bilgiler doğrulandıktan sonra isteğin kaynak sunucu tarafından işlenmesine izin verilir
 
-    return serverHttpSecurity.build();
+        return serverHttpSecurity.build();
     }
 }
--- discovery-server/src/main/java/org/example/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/org/example/discoveryserver/config/SecurityConfig.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -36,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest().authenticated() //Tüm HTTP isteklerinin kimlik doğrulaması gerektiğini belirtir.
                 .and()
                 .httpBasic(Customizer.withDefaults());
 
